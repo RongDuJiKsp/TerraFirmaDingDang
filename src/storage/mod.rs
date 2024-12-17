d4a41566c74f8365fb7425a22aad9828bb41df28
YuQiJaang <rdjksp@gmail.com>
2024-12-17 14:15:17 +0800

    comment

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,1 +1,2 @@
+// rec_save模块用于持久化KV对
 pub mod rec_save;
--- src/tf_serde/mod.rs
+++ src/tf_serde/mod.rs
@@ -1,3 +1,6 @@
+//operator模块为对tf数据结构的定义
 pub mod operator;
+//search_stack维护搜索状态
 pub mod search_stack;
+//stringify导出一个序列化宏
 pub mod stringify;
