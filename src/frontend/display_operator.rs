5a146d13fedacf4d7303a79c525d87cd770a4793
YuQiJaang <rdjksp@gmail.com>
2024-12-17 17:14:33 +0800

    优化一下display

--- src/frontend/display_operator.rs
+++ src/frontend/display_operator.rs
@@ -3,10 +3,10 @@ use std::error::Error;
 use std::io::Write;
 
 pub fn display_ops<W: Write>(v: &Vec<TFOperator>, w: &mut W) -> Result<(), Box<dyn Error>> {
-    writeln!(w, "操作步骤如下：")?;
+    writeln!(w, "------操作步骤如下：-------")?;
     for (idx, e) in v.iter().enumerate() {
         writeln!(w, "第{}步：{}", idx, e.chinese())?;
     }
-    writeln!(w, "操作完成")?;
+    writeln!(w, "------操作完成------------")?;
     Ok(())
 }
