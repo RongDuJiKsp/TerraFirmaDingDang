c43b125aadb6916fe0708c9e57ff5d9c47bcb636
YuQiJaang <rdjksp@gmail.com>
2024-12-17 18:43:42 +0800

    fix:约束越界

--- src/tf_serde/search_stack.rs
+++ src/tf_serde/search_stack.rs
@@ -9,12 +9,22 @@ impl SearchStack {
     pub fn push(&mut self, e: TFOperator) {
         self.stack.push(e);
         let len = self.stack.len();
+        let size = self.conditions.len() - self.conditions.iter().filter(|x| x.is_none()).count();
         for (idx, c) in self.conditions.iter().enumerate() {
             self.condition_ok[idx] = match c {
                 TFConditionOp::Last(o) => len >= 1 && o.eq(&self.stack[len - 1]),
                 TFConditionOp::LastSecond(o) => len >= 2 && o.eq(&self.stack[len - 2]),
                 TFConditionOp::LastThird(o) => len >= 3 && o.eq(&self.stack[len - 3]),
-                TFConditionOp::NotLast(o) => len > 0 && o.ne(&self.stack[len - 1]),
+                TFConditionOp::NotLast(o) => {
+                    len > 0
+                        && o.ne(&self.stack[len - 1])
+                        && len >= size
+                        && &&self.stack[len - size..len - 1]
+                            .iter()
+                            .filter(|x| o.eq(*x))
+                            .count()
+                            > 0
+                }
                 TFConditionOp::Any(o) => {
                     self.condition_ok[idx] || (len >= 1 && o.eq(&self.stack[len - 1]))
                 }
